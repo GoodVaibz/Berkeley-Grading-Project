84004eec3b5ea6f9736575cd34a37c72a5fa26ab
Vaibhav Srikaran <vaibhav430@gmail.com>
2016-05-22 23:51:29 -0500

    Starting Out

---
+++ data.py
@@ -0,0 +1,7 @@
+year1 = 2007
+year2 = 2015
+
+spring_file1 = 'spring_' + str(year1) + '.csv'
+spring_file2 = 'spring_' + str(year2) + '.csv'
+fall_file1 = 'fall_' + str(year1) + '.csv'
+fall_file2 = 'fall_' + str(year2) + '.csv'

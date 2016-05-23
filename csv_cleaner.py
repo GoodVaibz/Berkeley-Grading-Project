8cdebb10eb18763cc1c107b86ab54b98d5b48708
Vaibhav Srikaran <vaibhav430@gmail.com>
2016-05-23 09:32:45 -0500

    cleaned old data

---
+++ csv_cleaner.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+out_file = sys.argv[1][:-4] + '_clean.csv'
+csvwriter = csv.writer(open(out_file,'wb'))
+with open(sys.argv[1], 'rU') as csvfile:
+	csvreader = list(csv.reader(csvfile))
+	for row in csvreader[1:]:
+		new_row = list(row)
+		new_row[1] = new_row[1].strip(' ')
+		while new_row[1][0] == '0':
+			new_row[1] = new_row[1][1:]
+		if "R0" in new_row[1]:
+			new_row[1] = new_row[1][0] + new_row[1][3:]
+
+
+		csvwriter.writerow(new_row)
